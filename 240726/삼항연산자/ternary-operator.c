fc392f09a203bca809bec81e13bbd115114f1cf4
LEE <121864128+xstarmax@users.noreply.github.com>
2024-07-26 18:16:51 +0900

    Title: 삼항연산자, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240726/삼항연산자/ternary-operator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int point;
+    char[10] = result;
+
+    scanf("%d", &point);
+    result = (point == 100)? pass: failure;
+
+    printf("result");
+    return 0;
+}
