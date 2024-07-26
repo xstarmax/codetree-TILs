d7262167b294007afda725193c209df9ca6744b4
LEE <121864128+xstarmax@users.noreply.github.com>
2024-07-26 18:23:31 +0900

    Title: 삼항연산자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240726/삼항연산자 2/ternary-operator-2.c
+++ 240726/삼항연산자 2/ternary-operator-2.c
@@ -6,3 +6,10 @@ int main() {
     printf("%s", (a==1)?"t": "f"); //삼항 연산의 결과로 "t"랑 "f"가 반환됨.. => %s를 사용
     return 0;
 }
+
+/*
+	char ans = a == 1 ? 't' : 'f';
+	
+    // 출력
+	printf("%c", ans);
+*/
