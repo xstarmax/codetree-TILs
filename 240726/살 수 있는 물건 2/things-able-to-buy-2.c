52bb0526c90450ef5e23ab26adf43f098c7aadb1
LEE <121864128+xstarmax@users.noreply.github.com>
2024-07-26 18:42:50 +0900

    Title: 살 수 있는 물건 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240726/살 수 있는 물건 2/things-able-to-buy-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int book=3000, mask=1000, pen=500, money;
+    scanf("%d", &money);
+    if (money >= book)
+        printf("book");
+    else if (money >= mask);
+        printf("mask");
+    else if (money >= pen);
+        printf("pen");
+    else
+        printf("no");
+    return 0;
+}
