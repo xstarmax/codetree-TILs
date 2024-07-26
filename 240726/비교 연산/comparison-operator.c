609ba79ecd08ae93fe6dc7d71f3ad5bf36514339
LEE <121864128+xstarmax@users.noreply.github.com>
2024-07-26 19:32:33 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240726/비교 연산/comparison-operator.c
+++ 240726/비교 연산/comparison-operator.c
@@ -2,12 +2,12 @@
 
 int main() {
     int a,b;
-    scanf("%d %d",a,b);
-    printf("%d",a>=b);
-    printf("%d",a>b);
-    printf("%d",b>=a);
-    printf("%d",b>a);
-    printf("%d",a==b);
-    printf("%d",a!=b);
+    scanf("%d %d",&a,&b);
+    printf("%d\n",a>=b);
+    printf("%d\n",a>b);
+    printf("%d\n",b>=a);
+    printf("%d\n",b>a);
+    printf("%d\n",a==b);
+    printf("%d\n",a!=b);
     return 0;
 }
