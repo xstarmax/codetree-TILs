7bff996928e948d31cc05d47b36efda6a1334d54
LEE <121864128+xstarmax@users.noreply.github.com>
2024-07-26 19:51:07 +0900

    Title: 특정 조건 세 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240726/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.c
+++ 240726/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.c
@@ -4,7 +4,9 @@ int main() {
     int a,b,c;
     scanf("%d %d %d", &a, &b, &c);
 
-    if (a<b && a<c)
+    if (a<=b && a<=c) 
+    // 왜 '<'가 아니라 '<='??
+    //'최솟값'을 찾는거라서 다른 수랑 값이 같더라도 최솟값에 해당하면 ok
         printf("1 ");
     else
         printf("0 ");
