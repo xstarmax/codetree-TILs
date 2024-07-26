73f4afeb7e644e29eea459bf7478869cf84f134d
LEE <121864128+xstarmax@users.noreply.github.com>
2024-07-26 20:09:21 +0900

    Title: 숫자의 계절은, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240726/숫자의 계절은/season-of-num.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int m;
+    scanf("%d",&m);
+
+    if (m>=3 && m<=5)
+        printf("Spring");
+    else if (m>=6 && m<=8)
+        printf("Summer");
+    else if (m>=9 && m<=11)
+        printf("Fall");
+    else
+        printf("Winter");
+    return 0;
+}
