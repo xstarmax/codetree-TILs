ed8f38ec45d2d34df3a4d54c4bbb08c1cdb5db05
LEE <121864128+xstarmax@users.noreply.github.com>
2024-07-26 11:04:15 +0900

    Title: 길이 단위 변환하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240726/길이 단위 변환하기/change-length-unit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    double ft = 30.48, mi = 160934;
+    
+    printf("9.2ft = %.1lfcm\n", 9.2*ft);
+    printf("1.3mi = %.1lfcm\n", 1.3*mi);
+    
+    return 0;
+}
+
+/* 1 ft = 30.48 cm, 1mile = 160934 cm */
