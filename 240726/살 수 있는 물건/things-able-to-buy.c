a94748c860ec7f8c879c0a371a73d233fd380a58
LEE <121864128+xstarmax@users.noreply.github.com>
2024-07-26 18:31:24 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240726/살 수 있는 물건/things-able-to-buy.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int book=3000, mask = 1000, money;
+    scanf("%d",&money);
+
+    if (money >= book)
+        printf("book");
+    else if (money >= mask)
+        printf("mask");
+    return 0;
+}
