7d97ad2dc774d3ac231252138f931d24197901be
LEE <121864128+xstarmax@users.noreply.github.com>
2024-07-26 19:43:59 +0900

    Title: 시력 검사, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240726/시력 검사/eye-test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    double left,right;
+    scanf("%d %d",&left, &right);
+
+    if (left >= 1.0 && right >= 1.0)
+        printf("High");
+    else if (left >= 0.5 && right >= 0.5);
+        printf("Middle");
+    else
+        printf("Low");
+    return 0;
+}
