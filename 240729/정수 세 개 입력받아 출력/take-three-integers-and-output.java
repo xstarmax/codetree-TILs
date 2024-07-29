33b79110b3c3207627b71ddaff41fd1013cc1660
LEE <121864128+xstarmax@users.noreply.github.com>
2024-07-29 12:40:22 +0900

    Title: 정수 세 개 입력받아 출력, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240729/정수 세 개 입력받아 출력/take-three-integers-and-output.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b,c;        
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        System.out.printf("%d %d %d",a,b,c);
+    }
+}
