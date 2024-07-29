b2f9a8260fa1b0a4b014c6a444a211ef52725903
LEE <121864128+xstarmax@users.noreply.github.com>
2024-07-29 20:36:31 +0900

    Title: 배열 채우고 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240729/배열 채우고 출력하기/filling-array-and-print.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char arr[10];
+
+    for (int i = 0; i < 10; i++) {
+        // 개행 문자 무시
+        scanf(" %c", &arr[i]);
+    }
+
+    for (int i = 9; i >= 0; i--) {
+        printf("%c", arr[i]);
+    }
+    return 0;
+}
