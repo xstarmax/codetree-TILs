6f5f3062ab4fcb5365929d4004a33c5bcf11d273
LEE <121864128+xstarmax@users.noreply.github.com>
2024-07-29 20:21:24 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240729/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y, m, d;
+
+    // 입력
+    cin >> m;
+    cin.get();
+    cin >> d;
+    cin.get();
+    cin >> y;
+
+    // 출력
+    cout << y << "." << m << "." << d;
+    return 0;
+}
