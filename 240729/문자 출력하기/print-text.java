6a87d94125b4ee66965b948686262ad750bc77b1
LEE <121864128+xstarmax@users.noreply.github.com>
2024-07-29 19:48:47 +0900

    Title: 문자 출력하기, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240729/문자 출력하기/print-text.java
+++ 240729/문자 출력하기/print-text.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
+        // char c = sc.next().charAt(0);
         
         int i = 1;
         while (i < 9) {
