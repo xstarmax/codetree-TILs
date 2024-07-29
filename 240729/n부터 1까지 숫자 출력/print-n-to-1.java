5cf9622f64b11a81b8e1611ac49ce4d99bff78d8
LEE <121864128+xstarmax@users.noreply.github.com>
2024-07-29 19:32:35 +0900

    Title: n부터 1까지 숫자 출력, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240729/n부터 1까지 숫자 출력/print-n-to-1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = n;
+
+        while (i>= 1) {
+            System.out.print(i + " ");
+            i--;
+        }
+    }
+}
