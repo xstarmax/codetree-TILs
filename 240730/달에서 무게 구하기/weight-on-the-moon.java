64663166ea030264da5dc3be948f6589b5c6e2ce
LEE <121864128+xstarmax@users.noreply.github.com>
2024-07-30 23:53:05 +0900

    Title: 달에서 무게 구하기, Time: 98ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240730/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int c = 13;
+        double g = 0.165;
+
+        System.out.printf("%d * %f = %.6f", c, g, c*g);
+    }
+}
