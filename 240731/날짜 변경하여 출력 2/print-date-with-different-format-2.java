eea19a461a757123aeea343a896cd5c95d37b169
LEE <121864128+xstarmax@users.noreply.github.com>
2024-07-31 00:20:36 +0900

    Title: 날짜 변경하여 출력 2, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240731/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        sc.useDelimiter("-");
+        int mm = sc.nextInt();
+        int dd = sc.nextInt();
+        int yy = sc.nextInt();
+        System.out.printf("%d.%d.%d",yy,mm,dd);
+    }
+}
