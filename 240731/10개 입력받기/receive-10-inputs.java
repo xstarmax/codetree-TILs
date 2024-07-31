c9816d0812ca2d00459b630bbc698add91e0beb3
LEE <121864128+xstarmax@users.noreply.github.com>
2024-07-31 13:03:45 +0900

    Title: 10개 입력받기, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240731/10개 입력받기/receive-10-inputs.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[10];
+        int cnt=0, sum=0;
+
+        for (int i=0; i<10; i++) {
+            int val = sc.nextInt();
+            if (val == 0) {
+                break;
+            } else {
+                arr[i] = val;
+                sum += arr[i];
+                cnt++;
+            }
+        }
+        System.out.printf("%d %.1f", sum, (double)(sum/cnt));
+    }
+}
