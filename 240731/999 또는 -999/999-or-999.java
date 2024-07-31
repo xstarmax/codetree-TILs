7799ec9c3df318b4d336233876cb5d475a625d1e
LEE <121864128+xstarmax@users.noreply.github.com>
2024-07-31 15:35:11 +0900

    Title: 999 또는 -999, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240731/999 또는 -999/999-or-999.java
+++ 240731/999 또는 -999/999-or-999.java
@@ -4,20 +4,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int maxVal = Integer.MIN_VALUE;
-        int minVal = Integer.MAX_VALUE;
+        // 첫번째 입력에서 maxVal, minVal을 초기화
+        int maxVal, minVal;
+        maxVal = minVal = sc.nextInt();
 
+        // 두번째 입력부터 minVal, maxVal 초기화
         for (int i=0; i<100; i++) {
             int val = sc.nextInt();
-            if (val == 999 || val == -999)
-                break;
-            else {
-                if (val > maxVal)
-                    maxVal = val;
-                else if (val < minVal)
-                    minVal = val;
+            if (val > maxVal) {
+                maxVal = val;
+            }
+            else if (val < minVal) {
+                minVal = val;
             }
         }
+
+        // 최종 출력
         System.out.printf("%d %d", maxVal, minVal);
     }
 }
