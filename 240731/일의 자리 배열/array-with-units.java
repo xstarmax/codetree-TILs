296c2f9420b6444486c8845beb6d6b24b331d4d1
LEE <121864128+xstarmax@users.noreply.github.com>
2024-07-31 14:26:08 +0900

    Title: 일의 자리 배열, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240731/일의 자리 배열/array-with-units.java
+++ 240731/일의 자리 배열/array-with-units.java
@@ -4,15 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] arr = new int[11]; // 10번째 항을 위해 11번째까지 만듦
+        int[] arr = new int[10];
 
         arr[0] = sc.nextInt();
         arr[1] = sc.nextInt();
         
-        for (int i=0; i<9; i++) { 
-            arr[i+2] = arr[i] + arr[i+1];
+        for (int i=2; i<10; i++) { 
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+
+        for (int i=0; i<10; i++) {
             System.out.printf("%d ", arr[i] % 10);
         }
-        System.out.printf("%d ", arr[9] % 10);
     }
 }
