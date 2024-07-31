86025746847ffce53a0515bd0613427f7407c9bf
LEE <121864128+xstarmax@users.noreply.github.com>
2024-07-31 12:04:59 +0900

    Title: 평균점수 구하기, Time: 182ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240731/평균점수 구하기/print-average.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[] score = new double[8];
+        double sum=0;
+
+        for (int i=0; i<8; i++) {
+            score[i] = sc.nextDouble();
+            sum += score[i];
+        }
+
+        System.out.printf("%.1f", (sum/8));
+    }
+}
