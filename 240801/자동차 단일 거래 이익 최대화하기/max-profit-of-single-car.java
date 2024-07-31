819674fa733da237422d85066d9f18fcb8a5bbc3
LEE <121864128+xstarmax@users.noreply.github.com>
2024-08-01 00:07:41 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 240ms, Memory: 13MB, Status: Passed - CodeTree

--- 240801/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
+++ 240801/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -32,3 +32,28 @@ public class Main {
         
     }
 }
+
+/* 모범 답안
+        // 변수 선언:
+        int n;
+        int[] price = new int[MAX_NUM];
+
+        // 입력:
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            price[i] = sc.nextInt();
+
+        // 배열을 앞에서부터 순회하며 사는 시점의 후보를 선택합니다
+        int maxProfit = 0;
+        for(int i = 0; i < n; i++) {
+            // 사는 시점의 다음 해부터 순회하며 파는 시점의 후보를 선택합니다
+            for(int j = i + 1; j < n; j++) {
+                int profit = price[j] - price[i];
+                
+                if(profit > maxProfit)
+                    maxProfit = profit;
+            }
+        }
+
+        System.out.print(maxProfit);
+*/
