35a99ddf4bf413e44c7d644013386b90c339012c
LEE <121864128+xstarmax@users.noreply.github.com>
2024-08-07 23:27:37 +0900

    Title: Date to Date, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240807/Date to Date/date-to-date.java
+++ 240807/Date to Date/date-to-date.java
@@ -6,36 +6,24 @@ public class Main {
         // 윤년 제외
         int[] dbm = new int[]{0,31,28,31,30,31,30,31,31,30,31,30,31};
         
-
         // 입력 처리
         int m1 = sc.nextInt(); // 11
         int d1 = sc.nextInt(); // 5
         int m2 = sc.nextInt(); // 12
         int d2 = sc.nextInt(); // 21
 
-        // m1-d1 까지의 날짜수
-        int i=0;
-        int prev = 0;
-        int aft = 0;
-
-        while (true) {
-            if (i == m1-1)
-                break;
-            prev += dbm[i];
-            i++;
+        int pre = 0, aft = 0;
+        for (int i=0; i<m1; i++) {
+            pre += dbm[i];
         }
-        prev += d1;
+        pre += d1;
 
-        // m2-d2 까지의 날짜수
-        i=0;
-        while (true) {
-            if (i == m2-1)
-                break;
+        for (int i=0; i<m2; i++) {
             aft += dbm[i];
-            i++;
         }
         aft += d2;
-        
-        System.out.print(aft - prev);
+
+        // 출력 처리
+        System.out.print(aft - pre + 1);
     }
 }
