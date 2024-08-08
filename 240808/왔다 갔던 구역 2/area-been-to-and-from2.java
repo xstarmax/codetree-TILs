a9c13cb9184ce2e55fb1511ff0a8128dc7a82a18
LEE <121864128+xstarmax@users.noreply.github.com>
2024-08-08 21:18:16 +0900

    Title: 왔다 갔던 구역 2, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240808/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240808/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -1,39 +1,59 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main {    
+    public static final int OFFSET = 1000;
+    public static final int MAX_R = 2000;
+    public static final int MAX_N = 100;
+    
+    public static int n;
+    public static int[] x1 = new int[MAX_N];
+    public static int[] x2 = new int[MAX_N];
+    
+    public static int[] checked = new int[MAX_R + 1];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // 입력
+        n = sc.nextInt();
         
-        // 배열 만들기
-        int[] arr = new int[210]; // (-105 ~ 105)로 범위 잡음
-        int size = sc.nextInt();
-        int cur = 100; // 현재 위치
-
-        for (int i = 0; i < size; i++) {
-            int num = sc.nextInt();
-            char dir = sc.next().charAt(0);
-
-            if (dir == 'R') {
-                // 오른쪽으로 이동
-                for (int j = cur; j < cur + num; j++) {
-                    arr[j] += 1; // 방문 카운트 증가
-                }
-                cur += num; // 현재 위치 업데이트
-            } else if (dir == 'L') {
-                // 왼쪽으로 이동
-                for (int j = cur; j > cur - num; j--) {
-                    arr[j] += 1; // 방문 카운트 증가
-                }
-                cur -= num; // 현재 위치 업데이트
+        // 현재 위치
+        int cur = 0;
+        
+        for(int i = 0; i < n; i++) {
+            int distance = sc.nextInt();
+            char direction = sc.next().charAt(0);
+            
+            if(direction == 'L') {
+                // 왼쪽으로 이동할 경우 : cur - distance ~ cur까지 경로 이동
+                x1[i] = cur - distance;
+                x2[i] = cur;
+                cur -= distance;
             }
+            else {
+                // 오른쪽으로 이동할 경우 : cur ~ cur + distance까지 경로 이동
+                x1[i] = cur;
+                x2[i] = cur + distance;
+                cur += distance;
+            }
+            
+            // OFFSET을 더해줍니다.
+            x1[i] += OFFSET;
+            x2[i] += OFFSET;
         }
-
+        
+        // 구간을 칠해줍니다.
+        // 구간 단위로 진행하는 문제이므로
+        // x2[i]에 등호가 들어가지 않음에 유의합니다.
+        for(int i = 0; i < n; i++)
+            for(int j = x1[i]; j < x2[i]; j++)
+                checked[j]++;
+        
+        // 2번 이상 지나간 영역의 크기를 구합니다.
         int cnt = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 1) {
-                cnt++; // 두 번 이상 방문한 위치 카운트
-            }
-        }
+        for(int i = 0; i <= MAX_R; i++)
+            if(checked[i] >= 2)
+                cnt++;
+        
         System.out.print(cnt);
     }
 }
