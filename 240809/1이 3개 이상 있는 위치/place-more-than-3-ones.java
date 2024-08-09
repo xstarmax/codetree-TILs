30a02c5b6d0d1fda42f7c9eb298a7bd290235536
LEE <121864128+xstarmax@users.noreply.github.com>
2024-08-09 22:11:18 +0900

    Title: 1이 3개 이상 있는 위치, Time: 277ms, Memory: 18MB, Status: Passed - CodeTree

--- 240809/1이 3개 이상 있는 위치/place-more-than-3-ones.java
+++ 240809/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -1,43 +1,47 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_N = 100;
+    public static final int DIR_NUM = 4;
+
+    public static int n;
+    public static int[][] arr = new int[MAX_N][MAX_N];
+    
+    public static int[] dx = new int[]{0, 1,  0, -1};
+    public static int[] dy = new int[]{1, 0, -1,  0};
+
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
+
+    public static int adjacentCnt(int x, int y) {
+        int cnt = 0;
+        for(int i = 0; i < DIR_NUM; i++) {
+            int nx = x + dx[i], ny = y + dy[i];
+            if(inRange(nx, ny) && arr[nx][ny] == 1)
+                cnt++;
+        }
+        
+        return cnt;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        
-        int[][] arr = new int[size][size];
-        
-        // 입력 처리
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                arr[i][j] = sc.nextInt();
-            }
-        }
 
-        // 계산: 상하좌우 dx dy        
-        int[] dx = new int[]{0, 1, 0, -1};
-        int[] dy = new int[]{1, 0, -1, 0};
+        // 입력
+        n = sc.nextInt();
+
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                arr[i][j] = sc.nextInt();
         
-        int cnt = 0;  // 카운트 변수 초기화
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                int oneCount = 0;  // 인접한 1의 수 초기화
-                for (int dir = 0; dir < 4; dir++) {
-                    int di = i + dx[dir];
-                    int dj = j + dy[dir];
-                    // 인접한 칸이 격자 내에 있는지 확인
-                    if (di >= 0 && di < size && dj >= 0 && dj < size) {
-                        if (arr[di][dj] == 1) {
-                            oneCount++;
-                        }
-                    }
-                }
-                // 인접한 1의 수가 3개 이상인 경우 카운트
-                if (oneCount >= 3) {
-                    cnt++;
-                }
-            }
-        }
-        System.out.print(cnt);
+        // 각 칸을 탐색합니다.
+        int ans = 0;
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                if(adjacentCnt(i, j) >= 3)
+                    ans++;
+        
+        System.out.println(ans);
     }
 }
