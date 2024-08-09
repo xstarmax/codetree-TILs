9f4666016d953f7ea0a786dfcaec1fff8dd3c0fc
LEE <121864128+xstarmax@users.noreply.github.com>
2024-08-10 01:36:55 +0900

    Title: 최고의 13위치 2, Time: 114ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240810/최고의 13위치 2/best-place-of-13-2.java
+++ 240810/최고의 13위치 2/best-place-of-13-2.java
@@ -1,52 +1,36 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
+    public static final int MAX_N = 20;
 
-        int[][] grid = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                grid[i][j] = scanner.nextInt();
+    public static int n;
+    public static int[][] grid = new int[MAX_N][MAX_N];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = sc.nextInt();
             }
         }
 
         int maxCoins = 0;
-        for (int i = 0; i <= N - 3; i++) {
-            for (int j = 0; j < N; j++) {
-                int coins1 = countCoins(grid, i, j, i + 2, j);
-                for (int k = 0; k <= N - 3; k++) {
-                    for (int l = 0; l < N; l++) {
-                        if (k >= i && k <= i + 2 && l == j) continue;
-                        int coins2 = countCoins(grid, k, l, k + 2, l);
-                        maxCoins = Math.max(maxCoins, coins1 + coins2);
-                    }
-                }
-            }
-        }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j <= N - 3; j++) {
-                int coins1 = countCoins(grid, i, j, i, j + 2);
-                for (int k = 0; k < N; k++) {
-                    for (int l = 0; l <= N - 3; l++) {
-                        if (k == i && l >= j && l <= j + 2) continue;
-                        int coins2 = countCoins(grid, k, l, k, l + 2);
-                        maxCoins = Math.max(maxCoins, coins1 + coins2);
-                    }
-                }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                int coins = countCoins(i, j);
+                maxCoins = Math.max(maxCoins, coins);
             }
         }
 
         System.out.println(maxCoins);
     }
 
-    public static int countCoins(int[][] grid, int x1, int y1, int x2, int y2) {
+    public static int countCoins(int x, int y) {
         int coins = 0;
-        for (int i = x1; i <= x2; i++) {
-            for (int j = y1; j <= y2; j++) {
-                coins += grid[i][j];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                coins += grid[x + i][y + j];
             }
         }
         return coins;
